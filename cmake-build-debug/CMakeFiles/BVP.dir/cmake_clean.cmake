71a3a160605d87c37243e0f7a1e3ddabc118f7bc
zhengyugu <a1021604754@gmail.com>
2019-11-13 16:19:14 -0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/BVP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BVP"
+  "BVP.pdb"
+  "CMakeFiles/BVP.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BVP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
